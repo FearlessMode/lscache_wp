6ec0a3ff6b1f3306c05146b7889eb72a1f91b323
Hai Zheng <hzheng@litespeedtech.com>
2017-05-09 14:02:13 -0400

    [UPDATE] Use new const for wp-content path

--- litespeed-cache/admin/class-litespeed-cache-admin-rules.php
+++ litespeed-cache/admin/class-litespeed-cache-admin-rules.php
@@ -156,7 +156,7 @@ class LiteSpeed_Cache_Admin_Rules extends LiteSpeed{
 	 * @access private
 	 */
 	private function path_set(){
-		$this->theme_htaccess = WP_CONTENT_DIR;
+		$this->theme_htaccess = LSWCP_CONTENT_DIR;
 
 		$install = ABSPATH;
 		$access = get_home_path();
@@ -310,7 +310,7 @@ class LiteSpeed_Cache_Admin_Rules extends LiteSpeed{
 
 		// File put contents will truncate by default. Will create file if doesn't exist.
 		$ret = file_put_contents($path, $content, LOCK_EX);
-		if (!$ret) {
+		if ($ret === false) {
 			LiteSpeed_Cache_Admin_Display::add_error(LiteSpeed_Cache_Admin_Error::E_HTA_SAVE);
 			return false;
 		}
