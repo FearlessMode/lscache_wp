6d0f322bc4702b4a0d3f0c57a5de54fc9879e48e
Hai Zheng <hzheng@litespeedtech.com>
2017-06-07 14:02:13 -0400

    [BUGFIX] fixed tab switch compatibility in wp settings; fixed duplicated setting saved msg; added sitemap post type debug log

--- litespeed-cache/admin/class-litespeed-cache-admin-display.php
+++ litespeed-cache/admin/class-litespeed-cache-admin-display.php
@@ -559,7 +559,9 @@ class LiteSpeed_Cache_Admin_Display
 			require_once LSWCP_DIR . 'admin/tpl/network_settings.php';
 		}
 		else {
-			settings_errors();
+			if ( $_GET['page'] != 'litespeedcache' ) {// ls settings msg need to display manually
+				settings_errors();
+			}
 			require_once LSWCP_DIR . 'admin/tpl/settings.php';
 		}
 	}
